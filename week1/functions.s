5ea07f10f47a88d96936f0cd12ef2ed30bac31f9
Julian Müller <julian1mueller@gmx.de>
2026-04-10 11:11:45 +0200

    inner_product implementiert

--- week1/functions.s
+++ week1/functions.s
@@ -1,6 +1,33 @@
-.global _add
+.global _inner_product
 
-_add:
-    movl %edi, %eax   # erstes Argument -> eax
-    addl %esi, %eax   # zweites Argument addieren
-    ret
+_inner_product_cpp:
+    # x0 -> a
+    # x1 -> b
+    # w2 -> i_size
+
+    # w4 -> counter
+    # x8 -> sum
+
+    mov x8, #0          # Summe mit 0 initialisieren
+    mov w4, #0          # Counter mit 0 initialisieren
+
+    loop_start:
+        cmp w4, w2
+        b.ge loop_end           # Springe ans Ende der Schleife, falls counter >= i
+
+        ldr w5, [x0, w4, uxtw 2]    # w5 = a[counter] (Basisadresse + counter * 4)
+        ldr w6, [x1, w4, uxtw 2]    # w6 = b[counter]
+
+        umull x5, w5, w6        # a[counter] * b[counter] (Zielregister 64bit)
+
+        add x8, x8, x5          # sum += x5
+
+        add w4, w4, #1          # counter++
+
+        b loop_start            # Springe zurück zum Anfang
+    loop_end:
+    mov x0, x8          # Rückgabewert = sum       
+    ret                 # Return
+
+
+    
--- week1/main.cpp
+++ week1/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cstdint>
 
 // Deklaration der Assembly-Funktion
-extern "C" int add(int a, int b);
+extern "C" int64_t inner_product(uint32_t const *i_a, uint32_t const *i_b, uint32_t i_size);
 
 int main() {
-    int result = add(3, 4);
-    std::cout << "Result: " << result << std::endl;
+    // Test-Arrays
+    uint32_t a[] = {1, 2, 3, 4};
+    uint32_t b[] = {5, 6, 7, 8};
+    uint32_t size = 4;
+
+    // Aufruf der Funktion
+    int64_t result = inner_product(a, b, size);
+    std::cout << "Inner Product Result: " << result << std::endl;
+
+    // Erwartetes Ergebnis: 1*5 + 2*6 + 3*7 + 4*8 = 5 + 12 + 21 + 32 = 70
     return 0;
 }
